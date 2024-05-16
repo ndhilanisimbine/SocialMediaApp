096d1dc6c994f8b1664caeae04d2ea3588c3992d
ndhilanisimbine <158347299+ndhilanisimbine@users.noreply.github.com>
2024-05-16 21:52:34 +0200

    Add files via upload

---
+++ RandomUsersApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UsersView()
+        }
+    }
+}
---
+++ UserData.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+@MainActor
+class UserData: ObservableObject {
+    @Published var users: String = ""
+    
+    init() {
+        Task {
+            await loadUsers()
+        }
+    }
+    
+    func loadUsers() async {
+        do {
+            let users = try await UserFetchingClient.getUsers()
+            self.users = users
+        }
+        catch {
+            print(error)
+        }
+    }
+}
+
+
+
